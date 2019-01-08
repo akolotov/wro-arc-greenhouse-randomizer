105214831fd4cfc084c734a5d7174f947f17feab
Harrm <harrm@mail.ru>
2019-01-08 22:17:48 +0300

    Changed server port to 5000

--- src.transpiled/server/app.js
+++ src.transpiled/server/app.js
@@ -19,8 +19,6 @@ app.get('/field', async function (req, res, next) {
 
 app.use('/', _express.default.static(path.join('public'))); // Fire it up!
 
-app.listen(3000);
-console.log('Listening on port 3000'); // Console will print the message
-
-console.log('Server running at http://127.0.0.1:3000/');
+app.listen(5000);
+console.log('Listening on port 5000');
 //# sourceMappingURL=app.js.map
