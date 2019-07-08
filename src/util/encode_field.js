b7450fe0755acbcfa3f64d9ab0641b5bde4f7c88
hany606 <h.hamed.elanwar@gmail.com>
2019-07-08 12:16:40 +0300

    Modify the randomizor to conform with the latest version of the rules (23th May 2019)

--- src/util/encode_field.js
+++ src/util/encode_field.js
@@ -28,15 +28,24 @@ export default function encodeField(field) {
 
     // blue box
     let blueIdx = field.boxColors.indexOf("Blue");
-    res += encodeBox(field.boxes[blueIdx], nextIntIn(0, 2));
+    let blueBox = encodeBox(field.boxes[blueIdx], nextIntIn(0, 2));
 
     // first box
     let firstIdx = field.boxColors.indexOf(field.cubeColors[blueIdx]);
-    res += encodeBox(field.boxes[firstIdx], nextIntIn(0, 2));
+    let firstBox = encodeBox(field.boxes[firstIdx], nextIntIn(0, 2));
 
     // second box
     let secondIdx = field.boxColors.indexOf(field.cubeColors[firstIdx]);
-    res += encodeBox(field.boxes[secondIdx], nextIntIn(0, 2));
+    let secondBox = encodeBox(field.boxes[secondIdx], nextIntIn(0, 2));
+    
+    let tmpList = [blueBox, firstBox , secondBox];
+
+    let tmp = nextIntIn(0,3);
+    res += tmpList[tmp];
+    tmpList.splice(tmp, 1 );
+    tmp = nextIntIn(0,2);
+    res += tmpList[tmp];
+    res += tmpList[1-tmp];
 
     return res;
 }
