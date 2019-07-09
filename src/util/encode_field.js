197f554ed9fd7a2bd985624fec3642259af02971
hany606 <h.hamed.elanwar@gmail.com>
2019-07-09 12:46:03 +0300

    Merge is done

--- src/util/encode_field.js
+++ src/util/encode_field.js
@@ -40,23 +40,13 @@ export default function encodeField(field) {
     
     // randomize the order of boxes
     let boxes = [blueBox, firstBox , secondBox];
-<<<<<<< HEAD
-    
+
     let randomIdx = nextIntIn(0,3);
     res += boxes[randomIdx];
     boxes.splice(randomIdx, 1 );
     randomIdx = nextIntIn(0,2);
     res += boxes[randomIdx];
     res += boxes[1-randomIdx];
-=======
-
-    let randomIdx = nextIntIn(0,3);
-    res += tmpList[tmp];
-    tmpList.splice(tmp, 1 );
-    tmp = nextIntIn(0,2);
-    res += tmpList[tmp];
-    res += tmpList[1-tmp];
->>>>>>> b8b4436cbab29e1dde8b1c9dc9b4a6942e05ca83
 
     return res;
 }
