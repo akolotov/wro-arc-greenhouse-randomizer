cb9c2c890ec16686720f8e7f8c170acec2be2d98
Hany Hamed <h.hamed.elanwar@gmail.com>
2020-01-14 19:41:20 +0300

    Change the randomizor according to 2020 rules

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# wro2018-arc-randomizer
-A web tool to help in drawing procedure for the challenge "Smart Greenhouse" of Advanced Robotics Category on World Robot Olympiad 2018/19
+# wro2020-arc-randomizer
+A web tool to help in drawing procedure for the challenge "Smart Greenhouse" of Advanced Robotics Category on World Robot Olympiad 2020
--- src/frontend/qr_code_renderer.js
+++ src/frontend/qr_code_renderer.js
@@ -1,31 +1,51 @@
 'use strict';
 
 import encodeField from '../util/encode_field';
+import {randomSet} from "../util/random";
 const QRCode = require('qrcode');
 
 
 
 export default function createQR(field, code) {
-    let canvas = document.getElementById("qr-code");
+    let canvas = [];
+    canvas[0] = document.getElementById("qr-code-top-right");
+    canvas[1] = document.getElementById("qr-code-top-left");
+    canvas[2] = document.getElementById("qr-code-bottom-left");
+    canvas[3] = document.getElementById("qr-code-bottom-right");
 
-    let encoded_text = document.getElementById("encoded-field");
-    encoded_text.appendChild(document.createTextNode(code));
+    let qr_codes_keys = ["encoded-field-top-right", "encoded-field-top-left", "encoded-field-bottom-left", "encoded-field-bottom-right"];
+    let text_colors_per_fake_qr = ["#cc3300", "#006600", "#003366"];
 
-    QRCode.toCanvas(canvas, code,
-        {
-            width: 412,
-            height: 412,
-            color: { dark: "#000000", light : "#ffffff" },
-            errorCorrectionLevel: 'H'
-        },
-        function (error) {
-            if (error) console.error(error);
+
+    let random_value = randomSet(4,4);
+
+    for(let i = 1; i <= 4; i++){
+        let index = random_value[i-1] - 1;
+        
+        if(index == 0){
+            let true_encoded_field_text = document.getElementById("true-encoded-field");
+            true_encoded_field_text.appendChild(document.createTextNode(qr_codes_keys[i-1].slice(14,qr_codes_keys[i-1].length)));
         }
-    );
 
-    var ctx = canvas.getContext("2d");
-    ctx.lineWidth = "3";
-    ctx.strokeStyle = "black";
-    ctx.rect(5, 5, 402, 402);
-    ctx.stroke();
+        let encoded_text = document.getElementById(qr_codes_keys[i-1]);
+        encoded_text.appendChild(document.createTextNode(code[index]));
+        if(index != 0)
+            encoded_text.style.color = text_colors_per_fake_qr[index-1];
+
+
+        QRCode.toCanvas(canvas[i-1], code[index],
+            {
+                width: 226.77,
+                hight: 226.77,
+                color: { dark: "#000000", light : "#ffffff" },
+                errorCorrectionLevel: 'H',
+                margin: 0
+            },
+            function (error) {
+                if (error) console.error(error);
+            }
+        );
+    
+    }
+
 }
